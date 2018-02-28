3b7de3e530c47708598144c29192d3e83c599b6f
bchalk101 <boruch11@gmail.com>
2018-02-28 14:26:45 +1100

    Display a ONE

--- sevenSeg.go
+++ sevenSeg.go
@@ -1,6 +1,6 @@
 package sevenseg
 
-import  "github.com/stianeikeland/go-rpio"
+import "github.com/stianeikeland/go-rpio"
 
 type sevenSegDisplay interface {
 	Display(item string) error
@@ -8,8 +8,10 @@ type sevenSegDisplay interface {
 
 type sevenSeg struct {
 	toDisplay string
-	pinH RaspberryPiPin
-	pinD4 RaspberryPiPin
+	pinA      RaspberryPiPin
+	pinB      RaspberryPiPin
+	pinH      RaspberryPiPin
+	pinD4     RaspberryPiPin
 }
 
 const (
@@ -17,11 +19,21 @@ const (
 	ONE = "1"
 )
 
-
 func (s *sevenSeg) Display(item string) error {
 	s.toDisplay = item
-	s.pinH.WriteState(rpio.High)
-	s.pinD4.WriteState(rpio.High)
+	switch s.toDisplay {
+	case DOT:
+		s.pinA.WriteState(rpio.Low)
+		s.pinB.WriteState(rpio.Low)
+		s.pinH.WriteState(rpio.High)
+		s.pinD4.WriteState(rpio.High)
+	case ONE:
+		s.pinA.WriteState(rpio.High)
+		s.pinB.WriteState(rpio.High)
+		s.pinH.WriteState(rpio.Low)
+		s.pinD4.WriteState(rpio.High)
+	}
+
 	return nil
 }
 
--- sevenSeg_test.go
+++ sevenSeg_test.go
@@ -7,14 +7,19 @@ import (
 
 var displayTests = []struct {
 	display string
+	pinAState rpio.State
+	pinBState rpio.State
 	pinHState rpio.State
 	pinD4State rpio.State
 } {
-	{DOT, rpio.High, rpio.High},
+	{DOT, rpio.Low,rpio.Low, rpio.High, rpio.High},
+	{ONE, rpio.High, rpio.High, rpio.Low, rpio.High},
 }
 
 func TestDisplay(t *testing.T) {
 	sevenSeg := new(sevenSeg)
+	sevenSeg.pinA = new(mockRaspberryPiPinImpl)
+	sevenSeg.pinB = new(mockRaspberryPiPinImpl)
 	sevenSeg.pinH = new(mockRaspberryPiPinImpl)
 	sevenSeg.pinD4 = new(mockRaspberryPiPinImpl)
 
@@ -26,12 +31,20 @@ func TestDisplay(t *testing.T) {
 			t.Errorf("Not displaying correct item: Expected %s but got %s", testState.display ,sevenSeg.toDisplay)
 		}
 
+		if sevenSeg.pinA.ReadState() != testState.pinAState  {
+			t.Errorf("Display %v: Expected Pin A to be %v but was %v", testState.display, testState.pinAState, sevenSeg.pinA.ReadState())
+		}
+
+		if sevenSeg.pinB.ReadState() != testState.pinBState  {
+			t.Errorf("Display %v: Expected Pin B to be %v but was %v", testState.display, testState.pinBState, sevenSeg.pinB.ReadState())
+		}
+
 		if sevenSeg.pinH.ReadState() != testState.pinHState  {
-			t.Errorf("Expected Pin H to be %vbut was %s", testState.pinHState, sevenSeg.pinH.ReadState())
+			t.Errorf("Display %v: Expected Pin H to be %v but was %v", testState.display, testState.pinHState, sevenSeg.pinH.ReadState())
 		}
 
 		if sevenSeg.pinD4.ReadState() != testState.pinD4State {
-			t.Errorf("Expected Pin D4 to be %v but was %v", testState.pinD4State, sevenSeg.pinD4.ReadState())
+			t.Errorf("Display %v: Expected Pin D4 to be %v but was %v", testState.display, testState.pinD4State, sevenSeg.pinD4.ReadState())
 		}
 	}
 }
